858b96794878d0889352863827426059de06a5ee
kazuki.takeyama <kazuki.takeyama@rakuten.com>
2018-06-14 06:57:50 +0000

    title

--- app/Event.php
+++ app/Event.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     public $timestamps = false;
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
